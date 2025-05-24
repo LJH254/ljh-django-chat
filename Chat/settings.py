b1ec3b92a9c5a33bf09b4bc37a9ec02c6151e24e
LJH254 <lgleefreeman@icloud.com>
2025-05-24 20:35:49 +0800

    提交

--- Chat/settings.py
+++ Chat/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'envelope',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'wqy071118',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
