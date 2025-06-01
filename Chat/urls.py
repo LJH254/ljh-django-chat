8f54fd3cfb2e669a84628c332f3f02730626d5cb
LJH254 <lgleefreeman@icloud.com>
2025-06-01 20:40:20 +0800

    提交

--- Chat/urls.py
+++ Chat/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+
 from PChat import views
 
 urlpatterns = [
